679ba4697e36052c68ba10b507416cd683976137
Exppie <nr9691439@gmail.com>
2024-05-15 01:34:46 -0300

    implement inicial file and folder structure

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,14 @@
+//Este layout será usado para os principais componentes como Home, Dashboard, Trasactions...
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <main>
+        SIDEBAR
+        {children}
+    </main>
+  );
+}
